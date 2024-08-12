2e22f524fe61a2d2459f9b43db30de509b5936a9
vamsisai <vamsisai.karumuri@gmail.com>
2024-08-12 23:07:38 +0530

    fix:ui issue

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export  function Sidebar() {
   return (
     <aside className="w-full h-full  md:w-1/6  space-y-4 flex flex-col md:flex-col overflow-x-auto md:overflow-x-visible mt-4">
       <div className="flex flex-row md:flex-col md:space-y-4 space-x-4 md:space-x-0">
-          <div className="w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg shadow-md">
+          <div className="w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg drop-shadow-lg flex flex-col items-center justify-center">
             <div className="flex items-center gap-2 mb-2">
               <CircleAlertIcon className="h-6 w-6 text-red-500" />
               <span className="font-semibold text-muted-foreground">Expired</span>
@@ -16,7 +16,7 @@ export  function Sidebar() {
                 {tasks.filter(task => task.deadline < new Date().toISOString()).length}
             </div>
           </div>
-          <div className=" w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg shadow-md">
+          <div className=" w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg drop-shadow-lg flex flex-col items-center justify-center">
             <div className="flex items-center gap-2 mb-2">
               <ClipboardListIcon className="h-6 w-6 text-orange-500" />
               <span className="font-semibold text-muted-foreground">Active</span>
@@ -25,7 +25,7 @@ export  function Sidebar() {
                 {tasks.length}
             </div>
           </div>
-          <div className=" w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg shadow-md">
+          <div className=" w-1/3 h-1/2 md:w-auto p-4 bg-muted rounded-lg drop-shadow-lg flex flex-col items-center justify-center">
             <div className="flex items-center gap-2 mb-2">
               <CircleCheckIcon className="h-6 w-6 text-blue-500" />
               <span className="font-semibold text-muted-foreground">Completed</span>
